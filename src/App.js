ca099e5ba88c96d0a3b779dec87f465e7b2fffe9
promise-nathaniel <promise.nathaniel@tdafrica.com>
2022-05-11 13:37:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title={'You have 3 Todos'}/>
+  
+    </div>
+  
+      
+  );
+}
+
+export default App;
+
